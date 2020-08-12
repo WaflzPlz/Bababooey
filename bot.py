2517ff1c1c813f30df606eedd054091616326cab
WafflzPlz <michael.kogler1@gmx.at>
2020-08-12 11:48:58 +0200

    Added new sound effect, deleted old bot file and put sound effects in extra folder

--- bot.py
+++ bot.py
@@ -1,51 +1,70 @@
 import os
 import random
-
-import discord
 from dotenv import load_dotenv
+
+# 1
+import discord
+from discord.ext import commands
+from discord.voice_client import VoiceClient
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
 
-class Baba_Client(discord.Client):
-	async def on_ready(self):
-		print(f"{self.user} has connected to Discord!")
+# 2
+bot = commands.Bot(command_prefix="?")
 
-	async def on_member_join(self, member):
-		await member.create_dm()
-		await member.dm_channel.send(
-			f"Hi {member.name}, welcome to my Discord server!"
-		)
+@bot.event
+async def on_ready():
+	print(f"{bot.user.name} has connected to Discord!")
 
-	async def on_error(self, event, *args, **kwargs):
-		with open("err.log", "a") as f:
-			if event == "on_message":
-				f.write(f"Unhandled message: {args[0]}\n")
+@bot.command(name="join", help="Makes Bot join your coice chat")
+async def join(ctx):
+	author = ctx.message.author
+	channel = author.voice.channel
+	vc = await channel.connect()
 
-			else:
-				raise
+@bot.command(name="baba", help="Makes Bot play 'Bababooey'-sound effect")
+async def play(ctx):
+    guild = ctx.guild
+    vc = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio('soundfx/baba.mp3')
+    if not vc.is_playing():
+        vc.play(audio_source, after=None)
 
-	async def on_message(self, message):
-		if message.author == client.user:
-			return
+@bot.command(name="yankee", help="Makes Bot play 'Yankee with no brim'-sound effect")
+async def play(ctx):
+    guild = ctx.guild
+    vc = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio('soundfx/yankee_cut.mp3')
+    if not vc.is_playing():
+        vc.play(audio_source, after=None)
 
-		brooklyn_99_quotes = [
-			"I'm the human form of the 💯 emoji",
-			"Bingpot!",
-			(
-				"Cool. Cool cool cool cool cool cool cool,"
-				"no doubt no doubt no doubt no doubt."
-			),
-		]
+@bot.command(name="leave", help="Makes Bot leave the voice chat")
+async def leave(ctx):
+	guild = ctx.guild
+	vc = discord.utils.get(bot.voice_clients, guild=guild)
+	await vc.disconnect()
 
-		if message.content == "99!":
-			response = random.choice(brooklyn_99_quotes)
-			await message.channel.send(response)
+@bot.command(name="99!", help="Responds with a random quote from Brookly 99")
+async def nine_nine(ctx):
+	brooklyn_99_quotes = [
+		"I'm the human form of the 💯 emoji",
+		"Bingpot!",
+		(
+			"Cool. Cool cool cool cool cool cool cool,"
+			"no doubt no doubt no doubt no doubt."
+		),
+	]
 
-		elif message.content == "raise-exception":
-			raise discord.DiscordException
+	response = random.choice(brooklyn_99_quotes)
+	await ctx.send(response)
 
+@bot.command(name="roll_dice", help="Simulates rolling dice")
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+	dice = [
+		str(random.choice(range(1, number_of_sides + 1)))
+		for _ in range(number_of_dice)
+	]
+	await ctx.send(", ".join(dice))
 
-client = Baba_Client()
-client.run(TOKEN)
+bot.run(TOKEN)
