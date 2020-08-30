33fc8f7d4929a003fbb110f031928e9288dd3984
WafflzPlz <michael.kogler1@gmx.at>
2020-08-30 18:20:21 +0200

    added rockstar roast and removed unnecessary functions

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import os
 import random
 from dotenv import load_dotenv
 
-# 1
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
@@ -10,7 +9,6 @@ from discord.voice_client import VoiceClient
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-# 2
 bot = commands.Bot(command_prefix="?")
 
 @bot.event
@@ -45,26 +43,15 @@ async def leave(ctx):
 	vc = discord.utils.get(bot.voice_clients, guild=guild)
 	await vc.disconnect()
 
-@bot.command(name="99!", help="Responds with a random quote from Brookly 99")
-async def nine_nine(ctx):
-	brooklyn_99_quotes = [
-		"I'm the human form of the 💯 emoji",
-		"Bingpot!",
-		(
-			"Cool. Cool cool cool cool cool cool cool,"
-			"no doubt no doubt no doubt no doubt."
-		),
-	]
+@bot.event
+async def on_message(message):
+	if message.author == bot.user:
+		return
+	if "-play" and "rockstar" in message.content:
+		roast = f"get stick bugged! LOL {message.author.mention}\nposer"
+		channel = message.channel
+		await channel.send(roast)
 
-	response = random.choice(brooklyn_99_quotes)
-	await ctx.send(response)
-
-@bot.command(name="roll_dice", help="Simulates rolling dice")
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-	dice = [
-		str(random.choice(range(1, number_of_sides + 1)))
-		for _ in range(number_of_dice)
-	]
-	await ctx.send(", ".join(dice))
+	await bot.process_commands(message)
 
 bot.run(TOKEN)
